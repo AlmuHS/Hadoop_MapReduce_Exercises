104df4900e885510f37c8a613c4a82f2d71fba6b
Almudena Garcia <liberamenso10000@gmail.com>
2020-12-04 17:07:27 +0100

    change std::map to std::unordered_map

--- Practica1/reducer_csv.cpp
+++ Practica1/reducer_csv.cpp
@@ -1,8 +1,8 @@
 #include <string>
 #include <utility>
 #include <iostream>
-#include <map>
-
+#include <unordered_map>
+#include <algorithm>
 
 std::pair<std::string, float> get_key_value(std::string line){
 	size_t found = line.find_first_of("\t");
@@ -22,9 +22,9 @@ std::pair<std::string, float> get_key_value(std::string line){
 }
 
 
-std::map<std::string, float> calculate_avg(std::istream& file){
-	std::map<std::string, float> avg_map;
-	std::map<std::string, int> ocurrences_map;
+std::unordered_map<std::string, float> calculate_avg(std::istream& file){
+	std::unordered_map<std::string, float> avg_map;
+	std::unordered_map<std::string, int> ocurrences_map;
 	
  	std::string line;
  	
@@ -38,7 +38,7 @@ std::map<std::string, float> calculate_avg(std::istream& file){
 		std::string key = key_value.first;
 		float value = key_value.second;
 		
-		//Check if the key exists in the map
+		//Check if the key exists in the unordered_map
 		int exists = avg_map.count(key);
 		
 		//If exists, sum the new value
@@ -47,14 +47,14 @@ std::map<std::string, float> calculate_avg(std::istream& file){
 			int num_ocurrences = ocurrences_map[key];
 			avg_map[key] += value;
 		}
-		//If not exists, add the pair to the map
+		//If not exists, add the pair to the unordered_map
 		else{
 			avg_map.insert(key_value);
 		}
 	}
 	
 	//Calculate the average of each key
-	for(std::map<std::string,float>::iterator it = avg_map.begin(); it != avg_map.end(); ++it) {
+	for(std::unordered_map<std::string,float>::iterator it = avg_map.begin(); it != avg_map.end(); ++it) {
 	
 		//Get key and value
 		std::string key = it->first;
@@ -72,16 +72,23 @@ std::map<std::string, float> calculate_avg(std::istream& file){
 
 
 int main(void){
-	std::map<std::string, float> avg_map;
+	std::unordered_map<std::string, float> avg_map;
 	
 	//read file from standard input
 	std::istream* std_in = &std::cin;
 	avg_map = calculate_avg(*std_in);
 	
 	//write results to standard output
-	for(std::map<std::string,float>::iterator it = avg_map.begin(); it != avg_map.end(); ++it)
+	for(std::unordered_map<std::string,float>::iterator it = avg_map.begin(); it != avg_map.end(); ++it)
 	{
-		std::cout<<it->first<<"\t"<<it->second<<"\n";
+		std::string key = it->first;
+		float value = it->second;
+	
+		//remove "" from key name
+		key.erase(std::remove(key.begin(), key.end(), '\"'), key.end());
+		
+		//show the result
+		std::cout<<key<<"\t"<<value<<"\n";
 	}
 	
 	return 0;
