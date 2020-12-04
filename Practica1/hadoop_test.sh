6bfa4fcb63c23f532ef1f99494ec435a4664393e
Almudena Garcia <liberamenso10000@gmail.com>
2020-12-04 15:52:02 +0100

    add reducer to the script

--- Practica1/hadoop_test.sh
+++ Practica1/hadoop_test.sh
@@ -1,4 +1,5 @@
 hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
    -input RIA_exportacion_datos_diarios_Huelva_20140206.csv \
    -output output \
-   -mapper map_csv
+   -mapper map_csv \
+   -reducer reducer_csv
