4d7fa29378ae04ebbecd8f30f70c8d01a1afc51f
Almudena Garcia <liberamenso10000@gmail.com>
2020-12-05 18:25:46 +0100

    modified reducer to calculate average rain

--- Practica1/reducer_csv.cpp
+++ Practica1/reducer_csv.cpp
@@ -3,94 +3,99 @@
 #include <iostream>
 #include <unordered_map>
 #include <algorithm>
+#include <vector>
 
-std::pair<std::string, float> get_key_value(std::string line){
-	size_t found = line.find_first_of("\t");
-	std::pair<std::string, float> key_value;
-	
-	if(found != std::string::npos)
-	{
-		std::string key = line.substr(0,found);
-		std::string value = line.substr(found);
-		
-		key_value.first = key;
-		key_value.second = std::stof(value);
-	}
-	
-	return key_value;
+template <class Container>
+void split(const std::string& str, Container& cont, char delim = ' ')
+{
+    size_t start = 0;
+    size_t end = str.find(delim);
+
+    while (end != std::string::npos)
+    {
+        cont.push_back(str.substr(start, end-start));
+        start = end + 1;
+        end = str.find(delim, start);
+    }
+    cont.push_back(str.substr(start, end - start));
 }
 
+using map_pair = std::unordered_map<std::string, std::pair<float, float> >;
 
-std::unordered_map<std::string, float> calculate_avg(std::istream& file){
-	std::unordered_map<std::string, float> avg_map;
+
+map_pair calculate_avg(std::istream& file){
 	std::unordered_map<std::string, int> ocurrences_map;
+	map_pair avg_radiation_rain;
 	
  	std::string line;
  	
  	//Read file line to line
 	while(std::getline(file, line)){
 	
-		//Split the line in key and value
-		std::pair<std::string, float> key_value = get_key_value(line);
+		//Split the line in key and values
+		std::vector<std::string> values;
+		split(line, values, '\t');
 		
 		//extract key and value
-		std::string key = key_value.first;
-		float value = key_value.second;
+		std::string city = values[0];
+		float radiation = std::stof(values[1]);
+		float rain = std::stof(values[2]);
 		
 		//Check if the key exists in the unordered_map
-		std::unordered_map<std::string, float>::iterator it_key = avg_map.find(key);
+		map_pair::iterator it_key = avg_radiation_rain.find(city);
 		
 		//If exists, sum the new value
-		if(it_key != avg_map.end()){
+		if(it_key != avg_radiation_rain.end()){
 		
 			//Increase the number of ocurrences of this key
-			ocurrences_map[key]++;
+			ocurrences_map[city]++;
 			
-			//Sum the new value to this key
-			it_key->second += value;
+			//Sum the new values to this key
+			it_key->second.first += radiation;
+			it_key->second.second += rain;
 		}
 		//If not exists, add the pair to the unordered_map
 		else{
-			avg_map.insert(key_value);
+			avg_radiation_rain[city] = std::make_pair(radiation, rain);
 		}
 	}
 	
 	//Calculate the average of each key
-	for(std::unordered_map<std::string,float>::iterator it = avg_map.begin(); it != avg_map.end(); ++it) {
+	for(map_pair::iterator it = avg_radiation_rain.begin(); it != avg_radiation_rain.end(); ++it) {
 	
 		//Get key and value
-		std::string key = it->first;
-		float value = it->second;
+		std::string city = it->first;
+		float radiation = it->second.first;
+		float rain = it->second.second;
 		
 		//Find the number of ocurrences of this key in the file
-		int ocurrences = ocurrences_map[key];
+		int ocurrences = ocurrences_map[city];
 		
 		//Calculate the average
-		it->second = value/ocurrences;
+		it->second.first = radiation/ocurrences;
+		it->second.second = rain/ocurrences;
 	}
 	
-	return avg_map;
+	return avg_radiation_rain;
 }
 
 
 int main(void){
-	std::unordered_map<std::string, float> avg_map;
+	map_pair avg_map;
 	
 	//read file from standard input
 	std::istream* std_in = &std::cin;
 	avg_map = calculate_avg(*std_in);
 	
 	//write results to standard output
-	for(std::unordered_map<std::string,float>::iterator it = avg_map.begin(); it != avg_map.end(); ++it)
+	for (auto& map: avg_map)
 	{
-		std::string key = it->first;
-		float value = it->second;
-	
-		//remove "" from key name
-		key.erase(std::remove(key.begin(), key.end(), '\"'), key.end());
+		std::string city = map.first;
+		float radiation = map.second.first;
+		float rain = map.second.second;
 		
 		//show the result
-		std::cout<<key<<"\t"<<value<<"\n";
+		std::cout<<city<<"\t"<<radiation<<"\t"<<rain<<"\n";
 	}
 	
 	return 0;
