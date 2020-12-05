89e2f2435822984bda2e835a0a34e11051a887eb
Almudena Garcia <liberamenso10000@gmail.com>
2020-12-05 17:36:20 +0100

    update map to add rains value to the filter

--- Practica1/map_csv.cpp
+++ Practica1/map_csv.cpp
@@ -6,25 +6,43 @@
 #include <iterator>
 #include <regex>
 #include <locale>
+#include <unordered_map>
+
+#include <cstdio>
 
 const int RADIATION = 16;
+const int RAIN = 17;
 const int REGION = 1;
 const int CITY = 3;
 
-using vector_pair = std::vector<std::pair<std::string, float> >;
+using map_pair = std::unordered_map<std::string, std::pair<float, float> >;
 
 template <class Container>
 void split(const std::string& str, Container& cont, char delim = ' ')
 {
-    std::stringstream ss(str);
-    std::string token;
-    while (std::getline(ss, token, delim)) {
-        cont.push_back(token);
+    size_t start = 0;
+    size_t end = str.find(delim);
+
+    while (end != std::string::npos)
+    {
+        cont.push_back(str.substr(start, end-start));
+        start = end + 1;
+        end = str.find(delim, start);
     }
+    cont.push_back(str.substr(start, end - start));
 }
 
-vector_pair read_csv(std::istream& file){
-	vector_pair pair_list;
+std::string fix_empty_values(std::string value){
+	if(value == "" || value[0] == '\n' || value[0] == '\r'){
+		value = "0";
+	}
+	
+	return value;
+}
+
+
+map_pair read_csv(std::istream& file){
+	map_pair pair_list;
 	
  	std::string line;
  	
@@ -35,23 +53,28 @@ vector_pair read_csv(std::istream& file){
 	while(std::getline(file, line)){
 		std::vector<std::string> row;
 		
-		//replace empty fields with a blank character, to avoid errors in the split
-		line = std::regex_replace(line, std::regex(";;"), "; ;");
-		
 		//split the line by columns
 		split(line, row, ';');
 		
-		//Filter the radiation, together the city, of each city of Huelva. Ignore the rows with empty values
-		if(row[CITY] != " " && row[RADIATION] != " "){
-			pair_list.emplace_back(row[CITY], std::stof(row[RADIATION]));
-		}
+		//Filter the data about radiation and rain of this city
+		std::string city = row[CITY];
+		std::string radiation_str = row[RADIATION];
+		std::string rain_str = row[RAIN];
+		
+		radiation_str = fix_empty_values(radiation_str);
+		rain_str = fix_empty_values(rain_str);
+		
+		float rain = std::stof(rain_str);
+		float radiation = std::stof(radiation_str);
+		
+		pair_list[city] = std::make_pair(radiation, rain);	
 	}
 	
 	return pair_list;
 }
 
 int main(void){
-	vector_pair row_filtered;
+	map_pair row_filtered;
 	
 	//set locale to spanish. Necessary to read decimal point correctly
 	std::setlocale(LC_ALL, "es_ES.UTF-8");
@@ -62,7 +85,7 @@ int main(void){
 	
 	//write results to standard output
 	for (auto& row: row_filtered){
-		std::cout<<row.first<<"\t"<<row.second<<"\n";
+		std::cout<<row.first<<"\t"<<row.second.first<<"\t"<<row.second.second<<"\n";
 	}
 	
 	return 0;
