9a24b5938e62600162cbbc620867f01668a20715
Almudena Garcia <liberamenso10000@gmail.com>
2020-12-06 14:25:25 +0100

    reduce_radiation: change vector to pair to split key and value

--- Practica1/reduce_radiation.cpp
+++ Practica1/reduce_radiation.cpp
@@ -5,20 +5,7 @@
 #include <algorithm>
 #include <vector>
 
-template <class Container>
-void split(const std::string& str, Container& cont, char delim = ' ')
-{
-    size_t start = 0;
-    size_t end = str.find(delim);
-
-    while (end != std::string::npos)
-    {
-        cont.push_back(str.substr(start, end-start));
-        start = end + 1;
-        end = str.find(delim, start);
-    }
-    cont.push_back(str.substr(start, end - start));
-}
+#include "common.hpp"
 
 std::pair<std::string, float> calculate_max_radiation(std::istream& file){
 	std::pair<std::string, float> max_rad_pair;
@@ -28,16 +15,17 @@ std::pair<std::string, float> calculate_max_radiation(std::istream& file){
  	float max_rad = 0;
  	std::string city_max_rad;
  	
+ 	std::pair<std::string, float> city_radiation;
+ 	
  	//Read file line to line
 	while(std::getline(file, line)){
 	
 		//Split the line in key and values
-		std::vector<std::string> values;
-		split(line, values, '\t');
+		city_radiation = get_key_value(line);
 		
 		//extract key and value
-		std::string city = values[0];
-		float radiation = std::stof(values[1]);
+		std::string city = city_radiation.first;
+		float radiation = city_radiation.second;
 		
 		//Update max radiation
 		if(radiation > max_rad){
@@ -45,11 +33,10 @@ std::pair<std::string, float> calculate_max_radiation(std::istream& file){
 			city_max_rad = city;
 		}
 		
-		
 	}
 	
 	//Return a pair with the city of max radiation and its value
-	max_rad_pair = std::make_pair(city_max_rad, max_rad);
+	max_rad_pair = city_radiation;
 	
 	return max_rad_pair;
 }
