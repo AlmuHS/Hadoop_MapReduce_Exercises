05005e886011335f6e5ed5f6dd3b74d81ccf64af
Almudena Garcia <liberamenso10000@gmail.com>
2020-12-07 03:10:44 +0100

    map_rain: read output of previous job from file

--- Practica1/map_rain.cpp
+++ Practica1/map_rain.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 #include <iostream>
 #include <locale>
+#include <fstream>
 
 #include "common.hpp"
 
@@ -12,12 +13,9 @@ using vector_data = std::vector<std::pair<std::string, float> >;
 void filter_rain_csv(std::istream& file, std::string city_max_rad){	
  	std::string line;
  	
-	//Skip header
- 	std::getline(file, line);
- 	
  	//Read file line to line
 	while(std::getline(file, line)){
-		std::vector<std::string> row;
+		std::vector<std::string> row;	
 		
 		//split the line by columns
 		split(line, row, ';');
@@ -26,7 +24,7 @@ void filter_rain_csv(std::istream& file, std::string city_max_rad){
 		std::string city = row[CITY];
 			
 		if(city == city_max_rad){
-			//Replace empty values with zero
+			//Replace empty rain values with zero
 			std::string rain_str = fix_empty_values(row[RAIN]);
 			
 			//Extract year from date
@@ -43,19 +41,16 @@ int main(void){
 	//set locale to spanish. Necessary to read decimal separator correctly
 	std::setlocale(LC_ALL, "es_ES.UTF-8");
 	
-	//read file from standard input
+	//Read results from previous job
+	std::fstream output_radiation("output_radiation/part-00000", std::fstream::in);
+	std::string city_max_rad;
+	output_radiation>>city_max_rad;
+	
+	//read csv file from standard input
 	std::istream* std_in = &std::cin;
 	
-//	std::string line, city_max_rad;
-//	*std_in>>line;
-//	std::vector<std::string> city_rad;
-//	split(line, city_rad, '\t');
-//	city_max_rad = line[0];
-//	*std_in>>line;
-//	
-//	std::cout<<city_max_rad;
-	
-	filter_rain_csv(*std_in, "\"Lepe\"");
+	//Call to mapper function, passing the city with highest radiation by parameter
+	filter_rain_csv(*std_in, city_max_rad);
 	
 	return 0;
 }
