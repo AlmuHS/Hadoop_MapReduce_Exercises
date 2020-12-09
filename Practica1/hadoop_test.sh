7520bcdeaa47dac830156f2f6d46890da58860b8
Almudena Garcia <liberamenso10000@gmail.com>
2020-12-09 13:58:25 +0100

    modify path for single node mode

--- Practica1/hadoop_test.sh
+++ Practica1/hadoop_test.sh
@@ -1,14 +1,20 @@
-rm -rf output_radiation output_rain
+HADOOP_DIR="hdfs://0.0.0.0:9000/user/almu"
+
+hadoop fs -rm -r -f $HADOOP_DIR/output_radiation $HADOOP_DIR/output_rain
 
 hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
-   -input RIA_exportacion_datos_diarios_Huelva_20140206.csv \
-   -output output_radiation \
+   -input $HADOOP_DIR/Practica1/RIA_exportacion_datos_diarios_Huelva_20140206.csv \
+   -output $HADOOP_DIR/output_radiation \
    -mapper map_radiation \
    -combiner combiner_radiation \
    -reducer reduce_radiation
    
+hadoop fs -copyToLocal $HADOOP_DIR/output_radiation ./output_radiation
+   
 hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
-   -input RIA_exportacion_datos_diarios_Huelva_20140206.csv \
-   -output output_rain \
+   -input $HADOOP_DIR/Practica1/RIA_exportacion_datos_diarios_Huelva_20140206.csv \
+   -output $HADOOP_DIR/output_rain \
    -mapper map_rain \
    -reducer reducer_rain
+   
+hadoop fs -copyToLocal $HADOOP_DIR/output_rain ./output_rain
