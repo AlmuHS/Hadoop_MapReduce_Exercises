a1e5d2d308b1023cb1c26d0683759286d753b434
AlmuHS <almuhs@github.com>
2020-12-11 21:45:02 +0100

    fix path

--- Practica1/hadoop_test_stand.sh
+++ Practica1/hadoop_test_stand.sh
@@ -7,7 +7,7 @@ hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
    -reducer reduce_radiation
    
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
-   -input Practica1/RIA_exportacion_datos_diarios_Huelva_20140206.csv \
+   -input RIA_exportacion_datos_diarios_Huelva_20140206.csv \
    -output output_rain \
    -mapper map_rain \
    -reducer reducer_rain
