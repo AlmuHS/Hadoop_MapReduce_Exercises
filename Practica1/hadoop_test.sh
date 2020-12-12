d180e7720027f15a3a8f2dee0b6073831643957f
Almudena Garcia <liberamenso10000@gmail.com>
2020-12-12 21:08:13 +0100

    change installation directory if necessary

--- Practica1/hadoop_test.sh
+++ Practica1/hadoop_test.sh
@@ -1,23 +1,49 @@
-HADOOP_DIR="hdfs://0.0.0.0:9000/user/almu"
+USER="almu"
+HADOOP_DIR="hdfs://0.0.0.0:9000/user/$USER"
+
+#Change to Hadoop PseudoDistributed installation
+if test -d /usr/local/hadoop_std
+then
+	HADOOP_STD=$(grep hadoop_std ~/.bashrc | wc -l)
+	if test $HADOOP_STD == 1
+	then
+		sed -i '/HADOOP_HOME=/s|hadoop_std|hadoop|' ~/.bashrc
+		export HADOOP_HOME
+	fi
+fi
+
+#hadoop fs -copyFromLocal .
 
 hadoop fs -rm -r -f $HADOOP_DIR/output_radiation $HADOOP_DIR/output_rain
 
-hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
+START1=$(date +%s)
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
    -input $HADOOP_DIR/Practica1/RIA_exportacion_datos_diarios_Huelva_20140206.csv \
    -output $HADOOP_DIR/output_radiation \
    -mapper map_radiation \
    -reducer reduce_radiation
+END1=$(date +%s)
+
+time1=$(($END1 - $START1))
 
 rm -rf output_radiation
 
 hadoop fs -copyToLocal $HADOOP_DIR/output_radiation ./output_radiation
-   
-hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
+
+
+START2=$(date +%s)
+hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
    -input $HADOOP_DIR/Practica1/RIA_exportacion_datos_diarios_Huelva_20140206.csv \
    -output $HADOOP_DIR/output_rain \
    -mapper map_rain \
    -reducer reducer_rain
+END2=$(date +%s)
+
+time2=$(($END2-$START2))
 
 rm -rf output_rain
 
 hadoop fs -copyToLocal $HADOOP_DIR/output_rain ./output_rain
+
+total_time=$(($time1 + $time2))
+echo "Total time: $total_time scs"
--- Practica1/hadoop_test_stand.sh
+++ Practica1/hadoop_test_stand.sh
@@ -1,5 +1,25 @@
 rm -rf output_radiation output_rain
 
+#Change to Hadoop Standalone installation
+#Check if exists any Hadoop Standalone installation
+if test -d /usr/local/hadoop_std
+then
+	#If yes, check if the HADOOP_HOME is set to Hadoop Standalone path
+	HADOOP_STD=$(grep hadoop_std ~/.bashrc | wc -l)
+	
+	#If not, replace the HADOOP_HOME path in .bashrc
+	if test $HADOOP_STD == 0
+	then
+		#Replace HADOOP_HOME path to Hadoop Standalone installation
+		sed -i '/HADOOP_HOME=/s|hadoop|hadoop_std|' ~/.bashrc
+		
+		#Update variable to get the new path
+		export HADOOP_HOME
+	fi
+fi
+
+#Run the tasks
+START1=$(date +%s)
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
    -input RIA_exportacion_datos_diarios_Huelva_20140206.csv \
    -output output_radiation \
@@ -11,3 +31,8 @@ hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.10.1.jar \
    -output output_rain \
    -mapper map_rain \
    -reducer reducer_rain
+END1=$(date +%s)
+   
+time1=$(($END1 - $START1))
+
+echo "Total time: $time1 scs"
