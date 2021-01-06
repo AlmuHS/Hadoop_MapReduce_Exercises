d95ad08ecbc2feb01e268683239f2552173fab8c
Almudena Garcia <liberamenso10000@gmail.com>
2021-01-06 02:13:27 +0100

    apply some optimizations

--- Practica3/practica_spark.py
+++ Practica3/practica_spark.py
@@ -4,47 +4,57 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
+
+'''
+Configuracion: connect with spark local instance
+'''
+
 conf = (SparkConf().setMaster("local").setAppName("Practica3").set("spark.executor.memory","1g"))
 sc = SparkContext(conf= conf)
 spark = SparkSession(sc)
 
+
+'''
+Preprocessing stage: Read data from file, and fix format problems
+'''
+
+# Read data from file
 station_data = spark.read.options(delimiter=';', header='True') \
                     .csv("RIA_exportacion_datos_diarios_Huelva_20140206.csv")
 
-
-'''
-Task 1: 
-'''
 #Remove quotes and apply casting to integer
 remove_quotes = udf(lambda x : int(str(x).replace('\"', '')), IntegerType())
 station_fix_quotes = station_data.withColumn('IDESTACION',remove_quotes(station_data.IDESTACION))
 
-#Remove NULL values before casting radiation values
-radiation_fix_empty = station_fix_quotes.filter(station_fix_quotes.RADIACION.isNotNull())
+#Remove NULL values before casting values
+fix_empty = station_fix_quotes.fillna("0", subset=['PRECIPITACION', 'RADIACION'])
 
 #Fix decimal separator and apply casting to float
 commaToDot = udf(lambda x : float(str(x).replace(',', '.')), FloatType())
-radiation_fix_decimal = radiation_fix_empty.withColumn('RADIACION',commaToDot(radiation_fix_empty.RADIACION))
+radiation_fix_decimal = fix_empty.withColumn('RADIACION',commaToDot(fix_empty.RADIACION))
+rain_fix_decimal = radiation_fix_decimal.withColumn('PRECIPITACION',commaToDot(radiation_fix_decimal.PRECIPITACION))
 
-#Filter the stations which match with out IDs
-station_filtered = radiation_fix_decimal.filter((radiation_fix_decimal.IDESTACION > 1) & (radiation_fix_decimal.IDESTACION < 11))
+'''
+Task 1: Calculate the average radiation of a certain set of stations
+'''
+
+#Filter the stations which match with our station IDs
+station_filtered = rain_fix_decimal.filter((rain_fix_decimal.IDESTACION > 1) & (rain_fix_decimal.IDESTACION < 11))
 
 #Calculate the radiation average for each station
 avg_radiation = station_filtered.groupBy("SESTACION") \
                                 .avg("RADIACION") \
                                 .withColumnRenamed("avg(RADIACION)", "AVG_RADIACION")
 
-avg_radiation.show()
 avg_radiation.repartition(1).write.csv("output_radiation", sep='\t')
 
 '''
-Task 2:
+Task 2: From the station with highest average radiation, calculate the sum of rains for each year
 '''
 
 # Get the station with highest average radiation
 max_avg_radiation = avg_radiation.orderBy(col("AVG_RADIACION").desc()) \
                                  .limit(1)
-max_avg_radiation.show()
 
 # Filter the name of this station
 station_max_rad = max_avg_radiation.select(max_avg_radiation.columns[0])
@@ -53,23 +63,18 @@ name_smaxrad = station_max_rad.first()["SESTACION"]
 # Filter the data from this station
 station_filtered2 = station_filtered.filter(station_filtered.SESTACION == name_smaxrad)
 
-# Fix format problems
-rain_fix_empty = station_filtered2.fillna("0", subset=['PRECIPITACION'])
-rain_fix_decimal = rain_fix_empty.withColumn('PRECIPITACION',commaToDot(rain_fix_empty.PRECIPITACION))
-
 # Extract year from date
-rain_filter_year = rain_fix_decimal.withColumn('FECHA', rain_fix_decimal['FECHA'].substr(7,10)) \
+rain_filter_year = station_filtered2.withColumn('FECHA', station_filtered2['FECHA'].substr(7,10)) \
                                  .withColumnRenamed("FECHA", "ANIO")        
-rain_filter_year.show()
 
+# For this station, sum the rain for each year
 rain_year = rain_filter_year.groupBy("SESTACION", "ANIO") \
                             .sum("PRECIPITACION") \
                             .withColumnRenamed("sum(PRECIPITACION)", "TOTAL_PRECIPITACION") \
                             .select("SESTACION", "ANIO", "TOTAL_PRECIPITACION") \
                             .orderBy(col("ANIO").asc())
 
+# Group all partitions and save data to file
 rain_year.repartition(1).write.csv("output_rain", sep='\t')
-
-rain_year.show()
 
 
